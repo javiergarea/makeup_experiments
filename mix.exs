ad323ee93de1738395765889f705882a698c9475
Javier Garea <javier.garea@udc.es>
2020-03-21 15:52:54 +0100

    fix: add git tag based version

--- mix.exs
+++ mix.exs
@@ -1,17 +1,23 @@
 defmodule MakeupMarkdown.MixProject do
   use Mix.Project
 
-  @version "1.1.1"
   @url "https://github.com/javiergarea/makeup_markdown"
 
   def project do
     [
       app: :makeup_markdown,
-      version: @version,
+      version: version(),
       elixir: "~> 1.10.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       aliases: aliases()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {Makeup.Lexers.MarkdownLexer.Application, []}
     ]
   end
 
@@ -21,14 +27,7 @@ defmodule MakeupMarkdown.MixProject do
   defp aliases do
     [
       docs: &build_docs/1,
-      release: "run scripts/release.exs"
-    ]
-  end
-
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      mod: {Makeup.Lexers.MarkdownLexer.Application, []}
+      bump_version: "run scripts/bump_version.exs"
     ]
   end
 
@@ -40,6 +39,14 @@ defmodule MakeupMarkdown.MixProject do
     ]
   end
 
+  # Based on https://github.com/goodcodein/goodcode.in/blob/master/elixir/a-simple-way-to-automatically-set-the-semantic-version-of-your-elixir-app.md
+  def version do
+    # get git version
+    {git_desc, 0} = System.cmd("git", ~w[describe --tags])
+    ["v" <> major_minor, patch, _git_commit_id] = git_desc |> String.trim |> String.split("-")
+    "#{major_minor}.#{patch}"
+  end
+
   defp build_docs(_) do
     Mix.Task.run("compile")
     ex_doc = Path.join(Mix.path_for(:escripts), "ex_doc")
@@ -48,8 +55,8 @@ defmodule MakeupMarkdown.MixProject do
       raise "cannot build docs because escript for ex_doc is not installed"
     end
 
-    args = ["Makeup", @version, Mix.Project.compile_path()]
-    opts = ~w[--main Makeup --source-ref v#{@version} --source-url #{@url}]
+    args = ["Makeup", version(), Mix.Project.compile_path()]
+    opts = ~w[--main Makeup --source-ref v#{version()} --source-url #{@url}]
     System.cmd(ex_doc, args ++ opts)
     Mix.shell().info("Docs built successfully")
   end
--- test/makeup/lexers/markdown_lexer/markdown_lexer_test.exs
+++ test/makeup/lexers/markdown_lexer/markdown_lexer_test.exs
@@ -5,17 +5,17 @@ defmodule Makeup.Lexers.MarkdownLexer.MarkdownLexerTest do
 
   doctest MarkdownLexer
 
-  #property "lexes correctly texts" do
+  # property "lexes correctly texts" do
   #  check all(text <- CustomDataGen.text_gen()) do
   #    assert MarkdownLexer.lex(text) == [{:text, %{language: :markdown}, text}]
   #  end
-  #end
+  # end
 
-  #property "lexes correctly headings" do
+  # property "lexes correctly headings" do
   #  check all(heading <- CustomDataGen.heading_gen()) do
   #    assert MarkdownLexer.lex(heading) == [{:generic_heading, %{language: :markdown}, heading}]
   #  end
-  #end
+  # end
 
   test "trivial" do
     assert 1 == 1
