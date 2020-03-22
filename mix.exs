5772d0b990611cddd2c3618a210ad2574bf9dcb9
Javier Garea <javigarea@gmail.com>
2020-03-22 22:15:43 +0100

    fix(ci): github actions does not seem to be able to retrieve git tags in elixir setup phase

--- mix.exs
+++ mix.exs
@@ -1,12 +1,13 @@
 defmodule MakeupMarkdown.MixProject do
   use Mix.Project
 
+  @version "1.2.0"
   @url "https://github.com/javiergarea/makeup_markdown"
 
   def project do
     [
       app: :makeup_markdown,
-      version: version(),
+      version: @version,
       elixir: "~> 1.10.2",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -27,7 +28,8 @@ defmodule MakeupMarkdown.MixProject do
 
   defp aliases do
     [
-      docs: &build_docs/1
+      docs: &build_docs/1,
+      set_version: "run scripts/set_version.exs"
     ]
   end
 
@@ -39,13 +41,6 @@ defmodule MakeupMarkdown.MixProject do
     ]
   end
 
-  # Based on https://github.com/goodcodein/goodcode.in/blob/master/elixir/a-simple-way-to-automatically-set-the-semantic-version-of-your-elixir-app.md
-  defp version do
-    {git_desc, 0} = System.cmd("/usr/bin/git", ~w[describe --always --tags])
-    ["v" <> major_minor, _patch, _git_commit_id] = git_desc |> String.trim() |> String.split("-")
-    major_minor
-  end
-
   defp build_docs(_) do
     Mix.Task.run("compile")
     ex_doc = Path.join(Mix.path_for(:escripts), "ex_doc")
@@ -54,8 +49,8 @@ defmodule MakeupMarkdown.MixProject do
       raise "cannot build docs because escript for ex_doc is not installed"
     end
 
-    args = ["Makeup", version(), Mix.Project.compile_path()]
-    opts = ~w[--main Makeup --source-ref v#{version()} --source-url #{@url}]
+    args = ["Makeup", @version, Mix.Project.compile_path()]
+    opts = ~w[--main Makeup --source-ref v#{@version} --source-url #{@url}]
     System.cmd(ex_doc, args ++ opts)
     Mix.shell().info("Docs built successfully")
   end
