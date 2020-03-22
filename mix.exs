959f69bbb6a1ec9e349ffb834ac34b44ec12ec56
Javier Garea <javigarea@gmail.com>
2020-03-22 21:58:38 +0100

    ci: test

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule MakeupMarkdown.MixProject do
   # Based on https://github.com/goodcodein/goodcode.in/blob/master/elixir/a-simple-way-to-automatically-set-the-semantic-version-of-your-elixir-app.md
   def version do
     # get git version
-    {git_desc, 0} = System.cmd("git", ~w[describe --always --tags])
+    {git_desc, _} = System.cmd("git", ~w[describe --always --tags])
     ["v" <> major_minor, _patch, _git_commit_id] = git_desc |> String.trim() |> String.split("-")
     major_minor
   end
